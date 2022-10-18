ff3568bc47f430d379689ff3d251064794548d70
Mostafa Ellethy <mostafa.ellethy93@gmail.com>
2022-10-18 18:44:00 +0200

    Squashed commit of the following:
    commit 6fc549b8e1019d5713116c6fcc4ad54303baf9f3
    Author: Mostafa Ellethy <mostafa.ellethy93@gmail.com>
    Date:   Tue Oct 18 18:43:22 2022 +0200
    
        update: Landing Page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,50 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import styles from "../styles/Home.module.css";
+import { SiLinkedin, SiGithub, SiGmail } from "react-icons/si";
+
+const LINKS = [
+  {
+    icon: <SiLinkedin color="#0a66c2" />,
+    link: "https://www.linkedin.com/in/mostafaellethy/",
+    label: "LinkedIn",
+  },
+  {
+    icon: <SiGithub color="#24292f" />,
+    link: "https://github.com/MostafaEllethy",
+    label: "Github",
+  },
+  {
+    icon: <SiGmail color="#ea4335" />,
+    link: "mailto:mostafa.ellethy93@gmail.com",
+    label: "Email",
+  },
+];
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
+    <div className="p-2">
       <Head>
         <title>Mostafa Ellethy</title>
         <meta name="description" content="Mostafa Ellethy" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        <h1 className={styles.title}>MostafaEllethy.com</h1>
+      <main className="flex flex-1 h-screen flex-col items-center justify-center gap-3 sm:gap-5 md:gap-7">
+        <h1 className="text-center font-bold text-[2.825rem] sm:text-6xl md:text-7xl">
+          Mostafa Ellethy
+        </h1>
+        <section className="flex gap-4 md:gap-5 justify-center text-3xl sm:text-4xl">
+          {LINKS.map((l, idx) => (
+            <a
+              href={l.link}
+              key={idx}
+              target="_blank"
+              rel="noreferrer"
+              aria-label={l.label}
+            >
+              {l.icon}
+            </a>
+          ))}
+        </section>
       </main>
     </div>
   );
